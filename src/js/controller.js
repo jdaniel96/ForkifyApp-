616e1eee31b7361251d579e99c9be631064dc8cb
J.Daniel <juandaniel9619@gmail.com>
2022-06-29 15:00:13 -0400

    changes js

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,8 +1,9 @@
 import * as model from './model';
 import RecipeView from './views/recipeView.js';
 // import '/core-js/stable';
-import 'core-js/core';
-import './regenerator-runtime/runtime';
+import 'core-js/stable';
+// import '/regenerator-runtime/runtime';
+import 'regenerator-runtime/runtime';
 import searchView from './views/searchView';
 import ResultView from './views/resultView';
 import bookmarkView from './views/bookmarkView';
